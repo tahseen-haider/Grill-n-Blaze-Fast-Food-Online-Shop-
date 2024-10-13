1816debbc6b0c2e2fccb8ba2469853b31858f819
Tahsin Haider <tahsin3194@gmail.com>
2024-10-13 20:02:34 +0500

    s

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Layout from "../../components/Layouts/Layout";
 import Section1 from "./Section1";
+import Section2 from "./Section2";
 import '../../styles/HomeStyle.css'
 
 export default function Home() {
   return (
     <Layout>
       <Section1/>
+      <Section2/>
     </Layout>
   )
 }
