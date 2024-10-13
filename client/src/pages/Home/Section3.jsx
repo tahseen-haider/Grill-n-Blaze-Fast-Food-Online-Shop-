e6e94498c252d4e777779c811e8262bcf3e25257
Tahsin Haider <tahsin3194@gmail.com>
2024-10-13 10:43:40 +0500

    Section 2 Done

---
+++ client/src/pages/Home/Section3.jsx
@@ -0,0 +1,108 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+import Image1 from '../../assets/menu/burger-11.jpg'
+import Image2 from '../../assets/menu/burger-12.jpg'
+import Image3 from '../../assets/menu/burger-13.jpg'
+import Image4 from '../../assets/menu/burger-14.jpg'
+import Image5 from '../../assets/menu/burger-15.jpg'
+import Image6 from '../../assets/menu/burger-16.jpg'
+import Image7 from '../../assets/menu/burger-17.jpg'
+import Image8 from '../../assets/menu/burger-18.jpg'
+import Cards from "../../components/Layouts/Cards";
+
+const products = [
+    {
+      id: "0001",
+      image: Image1,
+      title: "Crispy Chicken",
+      paragraph: "Chicken breast, chilli sauce, tomatoes, pickles, coleslaw",
+      rating: 5,
+      price: 99.15,
+    },
+    {
+      id: "0002",
+      image: Image2,
+      title: "Ultimate Bacon",
+      paragraph: "House patty, cheddar cheese, bacon, onion, mustard",
+      rating: 4.5,
+      price: 99.32,
+    },
+    {
+      id: "0003",
+      image: Image3,
+      title: "Black Sheep",
+      paragraph: "American cheese, tomato relish, avocado, lettuce, red onion",
+      rating: 4,
+      price: 69.15,
+    },
+    {
+      id: "0004",
+      image: Image4,
+      title: "Vegan Burger",
+      paragraph: "House patty, cheddar cheese, bacon, onion, mustard",
+      rating: 3.5,
+      price: 99.25,
+    },
+    {
+      id: "0005",
+      image: Image5,
+      title: "Double Burger",
+      paragraph: "2 patties, cheddar cheese, mustard, pickles, tomatoes",
+      rating: 3.0,
+      price: 59.25,
+    },
+    {
+      id: "0006",
+      image: Image6,
+      title: "Turkey Burger",
+      paragraph: "Turkey, cheddar cheese, onion, lettuce, tomatoes, pickles",
+      rating: 3,
+      price: 79.18,
+    },
+    {
+      id: "0007",
+      image: Image7,
+      title: "Smokey House",
+      paragraph: "patty, cheddar cheese, onion, lettuce, tomatoes, pickles",
+      rating: 2.5,
+      price: 99.19,
+    },
+    {
+      id: "0008",
+      image: Image8,
+      title: "Classic Burger",
+      paragraph: "cheddar cheese, ketchup, mustard, pickles, onion",
+      rating: 2.0,
+      price: 89.12,
+    },
+    // Add more mock data objects as needed
+];
+
+export default function Section3() {
+  return (
+    <>
+        <section className="menu-section">
+            <Container>
+                <Row>
+                    <Col lg={{span:8, offset:2}}>
+                        <h2>Nothing brings people together like a good burger</h2>
+                        <p>Semper lacus cursus porta primis ligula risus tempus and
+                sagittis ipsum mauris lectus laoreet purus ipsum tempor enim
+                ipsum porta justo integer ultrice aligula lectus aenean magna
+                and pulvinar purus at pretium gravida</p>
+                    </Col>
+                </Row>
+                <Row>
+                    {products.map((item, index) => {
+                      return <Cards 
+                        key={index}
+                        
+                      />
+                    }
+                    )}
+                </Row>
+            </Container>
+        </section>
+    </>
+  )
+}
