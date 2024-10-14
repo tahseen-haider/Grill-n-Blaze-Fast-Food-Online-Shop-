c4b996d252c67fea2f6564bcfeb97d53a863d3b1
Tahsin Haider <tahsin3194@gmail.com>
2024-10-14 09:29:20 +0500

    Pulling in laptop

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,7 @@
 import Layout from "../../components/Layouts/Layout";
 import Section1 from "./Section1";
 import Section2 from "./Section2";
-import '../../styles/HomeStyle.css'
-import Section2 from "./Section2";
+import '../../styles/HomeStyle.css';
 import Section3 from "./Section3";
 import Section4 from "./Section4";
 import Section5 from "./Section5";
