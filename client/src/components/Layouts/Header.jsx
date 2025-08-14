034f31489692c22d5af7a40de7e6dd4e1d2fb32b
Tahsin Haider <tahsin3194@gmail.com>
2025-08-14 20:48:54 +0500

    navbar redesigned

--- client/src/components/Layouts/Header.jsx
+++ client/src/components/Layouts/Header.jsx
@@ -1,57 +1,57 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "/assets/logo/logo.png";
 import "../../styles/HeaderStyle.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Header() {
   const [nav, setNav] = useState(false);
 
-  const changeValueOnScroll = () => {
-    const scrollValue = document.documentElement.scrollTop;
-    scrollValue > 100 ? setNav(true) : setNav(false);
-  };
-
-  window.addEventListener("scroll", changeValueOnScroll);
+  useEffect(() => {
+    const changeValueOnScroll = () => {
+      setNav(document.documentElement.scrollTop > 100);
+    };
+    window.addEventListener("scroll", changeValueOnScroll);
+    return () => window.removeEventListener("scroll", changeValueOnScroll);
+  }, []);
 
   return (
     <header>
       <Navbar
         collapseOnSelect
         expand="lg"
-        className={`${nav === true ? "sticky" : ""}`}
+        className={`${nav ? "sticky" : ""} px-2 px-lg-5`}
       >
-        <Container>
-          <Link to="/" className="logo">
-            <img src={logo} alt="Logo" className="img-fluid" />
-          </Link>
+        {/* LEFT SIDE - Logo */}
+        <Link to="/" className="logo navbar-brand">
+          <img src={logo} alt="Logo" className="img-fluid" />
+        </Link>
+
+        {/* RIGHT SIDE (Mobile) - Cart + Toggler */}
+        <div className="d-flex align-items-center d-lg-none ms-auto gap-3">
+          <div className="cart me-2">
+            <i className="bi bi-cart2"></i>
+            <em className="roundpoint">2</em>
+          </div>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto">
-              <a href="#hero" className="nav-link">
-                Home
-              </a>
-              <a href="#about" className="nav-link">
-                About
-              </a>
-              <a href="#menu" className="nav-link">
-                Menu
-              </a>
-              <a href="#blog" className="nav-link">
-                Blog
-              </a>
-              <a href="#contact" className="nav-link">
-                Contact
-              </a>
-              <a to="/" className="nav-link">
-                <div className="cart">
-                  <i className="bi bi-cart2"></i>
-                  <em className="roundpoint">2</em>
-                </div>
-              </a>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        </div>
+
+        {/* CENTER (Desktop) */}
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto text-center">
+            <a href="#hero" className="nav-link">Home</a>
+            <a href="#about" className="nav-link">About</a>
+            <a href="#menu" className="nav-link">Menu</a>
+            <a href="#blog" className="nav-link">Blog</a>
+            <a href="#contact" className="nav-link">Contact</a>
+          </Nav>
+
+          {/* Cart for Desktop */}
+          <div className="cart d-none d-lg-flex ms-lg-3">
+            <i className="bi bi-cart2"></i>
+            <em className="roundpoint">2</em>
+          </div>
+        </Navbar.Collapse>
       </Navbar>
     </header>
   );
--- client/src/components/Layouts/Layout.jsx
+++ client/src/components/Layouts/Layout.jsx
@@ -1,12 +1,17 @@
+import PropTypes from 'prop-types';
 import Footer from "./Footer";
 import Header from "./Header";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <>
-      <Header/>
+      <Header />
       <div>{children}</div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
